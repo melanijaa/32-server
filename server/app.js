8a552a9ca03f4e49f4fa968dc520c9b672f8815f
Melanija <melanija.sibakovska1@gmail.com>
2022-06-15 11:00:15 +0300

    react and new files

--- server/app.js
+++ server/app.js
@@ -2,11 +2,47 @@ const express = require("express");
 const app = express();
 const port = 3003;
 
-//Routes
+const cors=require("cors");
+app.use(cors());
+const mysql=require("mysql");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "la_ma",
+});
+
+// Routes
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+res.send("Hello World!");
+});
+
+app.get("/zuikis", (req, res) => {
+ res.send("Labas, Zuiki!");
+});
+
+app.get("/medziai", (req, res) => {
+const sql = `
+SELECT
+*
+FROM trees
+`;
+con.query(sql, (err, result) => {
+if (err) throw err;
+res.send(result);
+});
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Bebras klauso port Nr. ${port}`);
 });
